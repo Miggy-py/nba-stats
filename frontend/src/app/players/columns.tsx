1dffb282ffd3fc2e97933efd333217bcb5d83354
miggy-py <mperezrojo23@gmail.com>
2025-08-06 08:57:46 -0400

    Updated table

--- frontend/src/app/players/columns.tsx
+++ frontend/src/app/players/columns.tsx
@@ -2,11 +2,23 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { Player } from "@/types/PlayerInterface";
+import { ArrowUpDown } from "lucide-react"
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Player>[] = [
     {
         accessorKey: 'DISPLAY_LAST_COMMA_FIRST',
-        header: 'Name'
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Name
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
     },
     {
         accessorKey: 'TEAM_NAME',
